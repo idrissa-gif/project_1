8e515f43e2419dc566ae4242b468f5502b2525dc
190041242_Idrissa_CSE_2B <idrissa@iut-dhaka.edu>
2021-07-23 16:39:22 +0600

    first commit

--- League.h
+++ League.h
@@ -1,9 +1,11 @@
 #pragma once
+#include<iostream>
+using namespace std;
 class League
 {
 private:
 	string name;
 	int no_team;
-
+	int match_day;
 };
 
--- Match.h
+++ Match.h
@@ -1,5 +1,7 @@
 #pragma once
+#include<iostream>
 #include"Player.h"
+using namespace std;
 
 class Match
 {
